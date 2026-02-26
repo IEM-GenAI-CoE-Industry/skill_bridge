022b44553a77a90a40e3e918c840029be8cd7137
madhurima sengupta <madhurimasengupta@madhurimas-MacBook-Air.local>
2026-02-26 23:51:14 +0530

    Added CV upload, loading state, skill verification modal

--- frontend/test/src/App.tsx
+++ frontend/test/src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
+import Features from "./pages/Features";
+import Stats from "./pages/Stats";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
       <Navbar />
-      <Landing />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
